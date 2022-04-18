a3ed92cb9ca3c6564f83062686e3ee0f47f7c301
jhaubova <96768412+jhaubova@users.noreply.github.com>
2022-04-18 09:34:53 -0800

    Add files via upload

--- python3 main.py
+++ python3 main.py
@@ -1,14 +1,21 @@
+#Project_3_Election_Sraper
+
+#Imports
 import csv
 import sys
 
 import bs4
 import requests
 
-
+#Get_territories
 def get_territories_csv_format(page):
     try:
-        # zakladni informace o obcich
+        #Basic_info
         base_infos = []
+        table = page.find("div", id="inner").find("table")
+        headers_appended = False
+        code_header = table.find("th", class_="fixed45", id="t1sb1").text.title()
+        name_header = table.find("th", class_="fixed150", id="t1sb2").text.title()
         for table in page.find("div", id="inner").find_all("table"):
             for tr in table.find_all("tr"):
                 code = tr.find("td", class_="cislo")
@@ -17,20 +24,23 @@ def get_territories_csv_format(page):
                 if code:
                     base_infos.append(
                         (code.a.text, name.text, "https://volby.cz/pls/ps2017nss/" + link.a['href']))
-        # uzemni celky se vsemi udaji
+        #Get_municipalities
         territories = []
         for base_info in base_infos:
             base_info_url = base_info[2]
             base_info_page = get_page(base_info_url)
             if base_info_page.find("div", class_="in_940"):
-                # for publikace_table in publikace_tables:
+                #For_publication_tables
                 territory = []
                 territory_links = base_info_page.find(
                     "div", id="publikace").find("table").find_all("td", class_="cislo")
                 if territory_links:
                     for territory_link in territory_links:
-                        territory1 = get_territory_csv_format(
-                            get_page("https://volby.cz/pls/ps2017nss/" + territory_link.a['href']), base_info)
+                        territory_page = get_page("https://volby.cz/pls/ps2017nss/" + territory_link.a['href'])
+                        if not headers_appended:
+                            territories.append(get_headers_csv_format(code_header, name_header, territory_page))
+                            headers_appended = True
+                        territory1 = get_territory_csv_format(territory_page, base_info)
                         if not territory:
                             territory = territory1
                             continue
@@ -40,6 +50,9 @@ def get_territories_csv_format(page):
                                     "".join(territory1[index].split())))
                 territories.append(territory)
             else:
+                if not headers_appended:
+                    territories.append(get_headers_csv_format(code_header, name_header, base_info_page))
+                    headers_appended = True
                 territories.append(get_territory_csv_format(base_info_page, base_info))
         return territories
     except:
@@ -59,7 +72,7 @@ def get_page(url):
     except:
         quit("Spatna URL")
 
-
+#Territories_basic_info
 def get_territory_csv_format(territory_page, base_info):
     table = territory_page.find("table")
     voters = table.find("td", class_="cislo", headers="sa2").text
@@ -79,6 +92,29 @@ def get_territory_csv_format(territory_page, base_info):
     for item in parties:
         territory_csv_format.append(item)
     return territory_csv_format
+
+#Headers
+def get_headers_csv_format(code_header, name_header, territory_page):
+    table = territory_page.find("table")
+    voters_header_br_tag = table.find("th", id="sa2").find("br")
+    envelops_header_br_tag = table.find("th", id="sa3").find("br")
+    valid_votes_header_br_tag = table.find("th", id="sa6").find("br")
+    voters_header = voters_header_br_tag.previous_sibling + " " + voters_header_br_tag.next_sibling
+    envelops_header = envelops_header_br_tag.previous_sibling + " " + envelops_header_br_tag.next_sibling
+    valid_votes_header = valid_votes_header_br_tag.previous_sibling + " " + valid_votes_header_br_tag.next_sibling
+    headers = [code_header, name_header, voters_header, envelops_header, valid_votes_header]
+    party_name_headers = []
+    parties_tables = territory_page.find("div", id="inner").find_all("table")
+    for table_index in range(len(parties_tables)):
+        parties_table = parties_tables[table_index]
+        for party in parties_table.find_all("tr"):
+            party_name_header = party.find("td", class_="overflow_name",
+                                           headers="t" + str(table_index + 1) + "sa1 t" + str(table_index + 1) + "sb2")
+            if party_name_header:
+                party_name_headers.append(party_name_header.text)
+    for item in party_name_headers:
+        headers.append(item)
+    return headers
 
 
 if __name__ == "__main__":
